6bad3762b09d469215f596504dd4d64eb5d9b823
philomagi <tooppoo56@gmail.com>
2022-01-10 03:41:13 +0900

    テスト追加

--- core/src/main/scala/philomagi/dddcj/modeling/planning_poker/core/attendance/model/Attendance.scala
+++ core/src/main/scala/philomagi/dddcj/modeling/planning_poker/core/attendance/model/Attendance.scala
@@ -12,23 +12,30 @@ case class Attendance(
 }
 object Attendance {
   case class Id(value: String) {
+    import philomagi.dddcj.modeling.planning_poker.core.attendance.lib.extension.StringExtension._
+
     require(value.nonEmpty, "attendance id must not be empty")
-    require(value.length <= Id.MaxLength, s"max length of attendance id is ${Id.MaxLength}")
+    require(value.nonBlank, "attendance id must not be blank")
+    require(value.within(Id.MaxLength), s"max length of attendance id is ${Id.MaxLength}")
+    require(value.matches("^[a-zA-Z0-9_-]+"), "id contains also invalid characters")
   }
   object Id {
     val MaxLength = 100
   }
 
   case class Name(value: String) {
+    import philomagi.dddcj.modeling.planning_poker.core.attendance.lib.extension.StringExtension._
+
     require(value.nonEmpty, "attendance name must not be empty")
-    require(value.length <= Name.MaxLength, s"max length of attendance name is ${Name.MaxLength}")
-    require(notIncludeInvalidCharacters(value), "attendance name include invalid character")
-
-    private def notIncludeInvalidCharacters(v: String): Boolean = {
-      val invalidCharactersForMemberName = "[<>\\\\%&$?+*^{}\\[\\]\t]".r
-
-      !invalidCharactersForMemberName.matches(v)
-    }
+    require(value.nonBlank, "attendance name must not be blank")
+    require(value.within(Name.MaxLength), s"max length of attendance name is ${Name.MaxLength}")
+    require(
+      value.notContains(Seq(
+        '<', '>', '%', '&', '?', '!', '+', '*', '/',
+        '^', '{', '}', '[', ']', '\t', '\\'
+      )),
+      "attendance name include invalid character"
+    )
   }
   object Name {
     val MaxLength = 50
---
+++ core/src/test/scala/philomagi/dddcj/modeling/planning_poker/core/attendance/model/AttendanceTest.scala
@@ -0,0 +1,110 @@
+package philomagi.dddcj.modeling.planning_poker.core.attendance.model
+
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.prop.TableDrivenPropertyChecks
+
+class AttendanceTest extends AnyFunSpec with TableDrivenPropertyChecks {
+  describe("Id") {
+    describe("generate") {
+      describe("with valid id") {
+        val testCases = Table(
+          ("バリエーション", "ID文字列"),
+          ("半角英1文字", "a"),
+          ("半角数1文字", "5"),
+          ("半角英のみ", "cicada"),
+          ("半角数のみ", "3301"),
+          ("半角英数のみ", "cicada3301"),
+          ("半角英数 + ハイフン", "cicada-3301"),
+          ("半角英数 + アンダーバー", "cicada_3301"),
+          ("半角英数 + ハイフン + アンダーバー", "cicada-_3301"),
+          ("ID長上限", "a" * Attendance.Id.MaxLength),
+        )
+
+        forAll(testCases) { (caseLabel, idValue) =>
+          describe(s"$caseLabel の場合") {
+            describe(s"id = \"$idValue\"") {
+              it("should generate id object") {
+                assert(Attendance.Id(idValue).value == idValue)
+              }
+            }
+          }
+        }
+      }
+      describe("with invalid id") {
+        val testCases = Table(
+          ("バリエーション", "ID文字列"),
+          ("空ID", ""),
+          ("ID長違反", "a" * (Attendance.Id.MaxLength + 1)),
+          ("半角空白のみ", "  "),
+          ("全角を含む", "cicadaてすと3310"),
+          ("半角空白を含む", "cicada 3310"),
+          ("全角空白を含む", "cicada　3310"),
+          ("記号を含む", "cicada/3310"),
+        )
+
+        forAll(testCases) { (caseLabel, invalidId) =>
+          describe(s"$caseLabel の場合") {
+            describe(s"id = \"$invalidId\"") {
+              it("should raise exception") {
+                assertThrows[IllegalArgumentException] {
+                  Attendance.Id(invalidId)
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  describe("Name") {
+    describe("generate") {
+      describe("with valid name") {
+        val testCases = Table(
+          ("バリエーション", "名前"),
+          ("半角1文字", "a"),
+          ("全角1文字", "松"),
+          ("半角上限", "a" * Attendance.Name.MaxLength),
+          ("全角上限", "あ" * Attendance.Name.MaxLength),
+          ("半角空白あり", "長宗我部 元親"),
+          ("全角空白あり", "長宗我部　元親"),
+          ("半角 + 全角", "伊藤 Mancio"),
+        )
+
+        forAll(testCases) { (caseLabel, name) =>
+          describe(s"$caseLabel") {
+            describe(s"name = $name") {
+              it("should generate name object") {
+                assert(Attendance.Name(name).value == name)
+              }
+            }
+          }
+        }
+      }
+      describe("with invalid name") {
+        val testCases = Table(
+          ("バリエーション", "名前"),
+          ("空文字", ""),
+          ("半角上限超", "a" * (Attendance.Name.MaxLength + 1)),
+          ("全角上限超", "あ" * (Attendance.Name.MaxLength + 1)),
+          ("半角空白のみ", "  "),
+          ("全角空白のみ", "　　"),
+          ("<混入", "伊藤 < Mahcio"),
+          (">混入", "伊藤 > Mahcio"),
+        )
+
+        forAll(testCases) { (caseLabel, name) =>
+          describe(s"$caseLabel") {
+            describe(s"name = $name") {
+              it("should not generate name object") {
+                assertThrows[IllegalArgumentException] {
+                  Attendance.Name(name)
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
